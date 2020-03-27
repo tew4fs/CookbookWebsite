8dccf098ef70750a3d69c9e21115bdfe4376dd10
tew4fs <43425253+tew4fs@users.noreply.github.com>
2020-03-26 22:57:53 -0500

    change db

--- Cookbook/settings.py
+++ Cookbook/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'Cookbook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kluybiab',
+        'USER': 'kluybiab',
+        'PASSWORD': 'd0Qbt0BTOEayK5Pdj2MUMDQMxGaYtbYl',
+        'HOST': 'drona.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
