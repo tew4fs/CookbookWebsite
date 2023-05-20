235d819d0ffe9e03644d5ac0ebcf6e0e4acaba0f
Trevor <tew4fs@virginia.edu>
2023-05-20 13:56:47 -0500

    remove test

--- recipe/tests.py
+++ recipe/tests.py
@@ -3,23 +3,3 @@ from recipe.models import Recipe
 
 
 # Create your tests here.
-class BreakfastRecipeTestCase(TestCase):
-    def setUp(self):
-        breakfast = Recipe.objects.create(
-            food="Breakfast Food",
-            food_type="breakfast",
-            description="A Breakfast Food",
-            cook_time=20,
-            serves=1,
-        )
-        breakfast.ingredients.add("Ingredient 1")
-
-    def testBreakfastRecipeWasAdded(self):
-        breakfast_recipes = Recipe.objects.filter(
-            food="Breakfast Food",
-            food_type="breakfast",
-            description="A Breakfast Food",
-            cook_time=20,
-            serves=1,
-        )
-        self.assertEqual(2, 1)
